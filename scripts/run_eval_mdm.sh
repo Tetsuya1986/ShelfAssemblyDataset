86c0b8860b1f9caa977a1193cc9dc3fa32ed5466
Tetsuya1986 <narita@mi.t.u-tokyo.ac.jp>
2026-06-27 01:13:46 +0000

    modified slurm exe script

--- scripts/run_eval_mdm.sh
+++ scripts/run_eval_mdm.sh
@@ -7,5 +7,5 @@
 #SBATCH --mail-user=narita@mi.t.u-tokyo.ac.jp
 #SBATCH -o log/stdout.%J
 #SBATCH -e log/stderr.%J
-python -m sample.eval_prediction --dataset shelf_assembly --task joint_motion_prediction --input_seconds 0.5 --prediction_seconds 1.0 --hml_mode action --model_path save/20260301_joint_motion_prediction_bert_50steps_action_18ld/model000240000.pt  --split test --num_repetitions 10 --autoregressive --autoregressive_include_prefix
+python -m sample.eval_prediction --dataset shelf_assembly --task collab_prediction --input_seconds 0.5 --prediction_seconds 1.0 --label_option action --model_path save/20260626_collab_prediction_bert_50steps_action_envcam_1.0_1dzg/model000170000.pt  --split test --num_repetitions 10 --autoregressive --autoregressive_include_prefix --use_envcam
 
